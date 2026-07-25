faf16dfda939e2f92a86f1c9bb0e316e1a283c51
Moiz Ul Hasan <moiz.hasan@ext.beyond.one>
2026-07-25 16:24:16 +0500

    Initial files + gitignore

---
+++ Gamecastic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gamecastic
+//
+//  Created by Moiz Ul Hasan on 25/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gamecastic/GamecasticApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamecasticApp.swift
+//  Gamecastic
+//
+//  Created by Moiz Ul Hasan on 25/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct GamecasticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
